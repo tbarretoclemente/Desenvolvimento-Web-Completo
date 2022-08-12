5a25f51bf307844a3444411b8e135f7cacab2473
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-08-11 21:08:22 -0300

    atualizado

---
+++ javascript/assets/js/desafios.js
@@ -0,0 +1,37 @@
+// DESAFIO 1
+
+// var a = 10;
+// var b = 20;
+// var c = null;
+// document.write("A: "+ a + "\nB: "+ b + " <br>");
+// console.log(a,b,c);
+// c = a;
+// a = b;
+// b = c;
+// document.write("A: "+ a + "\nB: "+ b);
+// console.log(a,b,c);
+
+
+// DESAFIO 2 = MEDIA ALUNO
+
+// var nota1 = 9.99;
+// var nota2 = 9.99;
+// var nota3 = 9.99;
+var nota1 = Number(prompt("Informe a primeira nota: "));
+var nota2 = Number(prompt("Informe a segunda nota: "));
+var nota3 = Number(prompt("Informe a terceira nota: "));
+
+// nota1 = parseFloat(nota1);
+// nota2 = parseFloat(nota2);
+// nota3 = parseFloat(nota3);
+
+var media = (nota1 + nota2 + nota3) / 3;
+if(media <= 4.99){
+    document.write("Aluno Reprovado !!!");
+}else if(media >= 5 && media <= 6.99){
+    document.write("Aluno Está de recuperação !!!");
+}else{
+    document.write("Aluno Aprovado !!!");
+}
+document.write("<br>Nome Final: " + media)
+console.log(media);
---
+++ javascript/assets/js/script.js
@@ -0,0 +1,19 @@
+// alert("Olá mundo!!");
+
+// Declaração de variaveis
+
+var nome = "Thiago Barreto Clemente";
+let rg = 337861892;
+const cpf = true;
+console.log(nome,rg, cpf);
+
+
+// IF/ELSE
+var verdaeiro = false;
+
+if(23 !== 22){
+    document.write("Esse é um if verdadeiro");
+}else{
+    document.write("Esse é um if falso");
+}
+
