69f27c538e4b2fc58a8480922ea456b93fe6ab9b
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-08-16 19:17:27 -0300

    atualizado

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -2,18 +2,79 @@
 
 // Declaração de variaveis
 
-var nome = "Thiago Barreto Clemente";
-let rg = 337861892;
-const cpf = true;
-console.log(nome,rg, cpf);
+// var nome = "Thiago Barreto Clemente";
+// let rg = 337861892;
+// const cpf = true;
+// console.log(nome,rg, cpf);
 
 
 // IF/ELSE
-var verdaeiro = false;
+// var verdaeiro = false;
 
-if(23 !== 22){
-    document.write("Esse é um if verdadeiro");
-}else{
-    document.write("Esse é um if falso");
-}
+// if(23 !== 22){
+//     document.write("Esse é um if verdadeiro");
+// }else{
+//     document.write("Esse é um if falso");
+// }
 
+
+// OPERADOR TERNÁRIO
+// var age = 12;
+// var resultado = (age > 18) ? true : false;
+// document.write(resultado);
+
+// SWITCH
+
+// var condicacao = 1;
+
+// switch(condicacao){
+//     case 1:
+//         if(condicacao == 1){
+//             document.write("Estamos no case 1");
+//         }
+//         break;
+//     case 2:
+//         if(condicacao == 2){
+//             document.write("Estamos no case 2");
+//         }
+//         break;
+
+//     default :
+//         document.write("Opção Default");
+//         break;
+// }
+
+
+// var condicacao2 = "sair";
+// switch (condicacao2) {
+//     case "start":
+//         document.write("O jogo vai começar");
+//         break;
+//     case "pausa":
+//         document.write("O jogo está em pausa");
+//         break;
+//     case "sair":
+//         document.write("O jogo foi fechado");
+//         break;
+
+//     default:
+//         alert("Default");
+//         break;
+// }
+
+
+// Operadores Aritmeticos
+
+// var numero1 = 10;
+// var numero2 = 10;
+
+// document.write(`A soma entre ${numero1} e ${numero2} = ${numero1 + numero2} <br>`);
+// document.write(`A subtração entre ${numero1} e ${numero2} = ${numero1 - numero2} <br>`);
+// document.write(`A multiplicação entre ${numero1} e ${numero2} = ${numero1 * numero2} <br>`);
+// document.write(`A divisão entre ${numero1} e ${numero2} = ${numero1 / numero2} <br>`);
+// document.write(`O resto da divisão entre ${numero1} e ${numero2} = ${numero1 % numero2} <br>`);
+// document.write(`A exponenciação/ou potenciação  entre ${numero1} e ${numero2} = ${numero1 ** numero2} <br>`);
+// document.write(`O incremento de ${numero1++} é `);
+// document.write(` ${numero1}<br>`);
+// document.write(`O decremento de ${numero2--} é `);
+// document.write(` ${numero2}<br>`);
