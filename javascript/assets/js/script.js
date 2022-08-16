0eb01a49528cf4763dfa6429f31c467239c249d5
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-08-16 20:39:15 -0300

    atualizado

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -78,3 +78,73 @@
 // document.write(` ${numero1}<br>`);
 // document.write(`O decremento de ${numero2--} é `);
 // document.write(` ${numero2}<br>`);
+
+
+// LOOPS 
+
+// WHILE
+// let contador = 0;
+// let contador1 = 10;
+
+// document.write("<h3>Usando Loop WHILE para criar contador</h3>");
+
+
+// while(contador < 11){
+//     document.write(`Contador de Incremento ${contador} <br>`);
+//     contador++;
+// }
+
+// document.write("<hr>")
+// while(contador1 > -1){
+//     document.write(`Contador de Decremento ${contador1} <br>`);
+//     contador1--;
+// }
+// document.write("<hr>")
+// document.write("<h3>Usando Loop FOR para criar contador</h3>");
+
+// FOR
+
+// for(contadorFor = 0; contadorFor < 11; contadorFor++){
+//     document.write(`Contador de Incremento ${contadorFor} <br>`); 
+// }
+
+// document.write("<hr>")
+// for(contadorFor1 = 10; contadorFor1 > -1; contadorFor1--){
+//     document.write(`Contador de Decremento ${contadorFor1} <br>`);
+// }
+
+
+// FUNÇÕES
+// let nomeUsuario = "Thiago Barreto Clemente";
+// let num1 = Number(prompt("Digite um número: "));
+// let num2 = Number(prompt("Digite um número: "));
+
+// function helloWorld(){
+//     alert("Hello World!!!")
+// }
+
+// function texto(){
+//     let texto = "Essa é uma função usando variavel local de retorno!!!";
+//     alert(texto);
+// }
+
+// function boasVindas(){
+//     let texto = `Olá ${nomeUsuario} seja bem vindo ao javascript`;
+//     alert(texto)
+// }
+
+// function parametros(text, nome){
+//     alert(text + nome);
+// }
+
+// function retorno(numero1, numero2){
+//     let soma = numero1 + numero2;
+//     return soma;
+// }
+// resultadoSoma = retorno(num1,num2)
+// document.write(`A soma entre os números é: ${num1} e ${num2} = ${resultadoSoma}`);
+
+// parametros("Olá ", nomeUsuario);
+// boasVindas();
+// helloWorld();
+// texto();
