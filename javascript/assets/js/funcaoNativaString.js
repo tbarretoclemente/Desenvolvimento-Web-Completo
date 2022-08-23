6dcf69703f2474e3db8f2e3ae42b840c36b72f87
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-08-23 20:20:30 -0300

    atualizado

---
+++ javascript/assets/js/funcaoNativaString.js
@@ -0,0 +1,8 @@
+let nome = "Thiago Barreto Clemente"
+document.write(`<h1>Aula - Funções Nativas JavaScript - STRINGS</h1>`);
+document.write(`<strong>Método Length:</strong> Usada para encontrar o comprimento de uma string`);
+document.write(`<br>STRING: ${nome} // RETORNO DO MÉTODO: ${nome.length} <br><br>`);
+document.write(`<strong>Método charAt:</strong> Retorna o caractere em um índice especificado.`);
+document.write(`<br>STRING: ${nome} // RETORNO DO MÉTODO: ÍNDICE ${nome.indexOf('B')}, Letra ${nome.charAt(7)}.<br><br>`)
+document.write(`<strong>Método IndexOf:</strong> Retorna o índice da primeira ocorrência de um valor em uma string.`);
+document.write(`<br>STRING: ${nome} // RETORNO DO MÉTODO: ÍNDICE ${nome.indexOf('a')}, Letra ${nome.charAt(3)}.<br><br>`)
