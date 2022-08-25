fafce0d6acfdc1456e70f659ef12180345fbe912
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-08-25 15:33:42 -0300

    atualizado

---
+++ javascript/assets/js/eventoMouse.js
@@ -0,0 +1,27 @@
+// EVENTOS MAIS USADOS PELO MOUSE
+// onclick
+// ondblclick
+// onmouseup
+// onmouseover
+// onmouseout
+
+
+function funcOnclick(){
+    alert(`Evento onclick !!!`);
+}
+
+function funcOndblclick(){
+    alert(`Evento ondblclick !!!`);
+}
+
+function funcOnmouseup(){
+    alert(`Evento onmouseup !!!`);
+}
+
+function funcOnmouseover(){
+    alert(`Evento onmouseover !!!`);
+}
+
+function funcOnmouseout(){
+    alert(`Evento onmouseout !!!`);
+}
