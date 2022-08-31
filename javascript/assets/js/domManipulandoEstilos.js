dc67fdb279cac844b8fa095750e1db93d70a1b49
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-08-31 16:40:20 -0300

    atualizado

---
+++ javascript/assets/js/domManipulandoEstilos.js
@@ -0,0 +1,6 @@
+// MANIPULANDO ESTILOS COM JS
+
+function estilo(cor){
+    document.getElementById("box").style.background = cor;
+    document.getElementById("box").style.transition = "all 3s";
+}
