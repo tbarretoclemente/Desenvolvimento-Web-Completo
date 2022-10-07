dc9d490bef50ef3fa413d94c936dc6e8bfe4f3dd
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-10-07 01:48:28 -0300

    atualizado

--- ecmascript-2015-a-2021/app-Orcamento-Pessoal/assets/js/app.js
+++ ecmascript-2015-a-2021/app-Orcamento-Pessoal/assets/js/app.js
@@ -12,16 +12,61 @@ btnMobile.addEventListener("click", () =>{
 
 
 
+// Class Servidor
+
+// class Servidor {
+//     constructor(ano,mes,dia,tipo,descricao,valor){
+//         this.anoID = ano;
+//         this.mesID = mes;
+//         this.diaID = dia;
+//         this.tipoID = tipo;
+//         this.descricaoID = descricao;
+//         this.valorID = valor;
+//     }
+// }
+
+
+class Servidor {
+    constructor(){
+
+    }
+
+
+    // METODO
+
+    getValoresInput(dados){
+        console.log(dados);
+        return dados;
+    }
+}
+
+
 // RECUPERANDO VALORES DOS INPUT'S
 
 
 const valoresInputs = document.getElementById("btnAdd");
+// valoresInputs.addEventListener("click", ()=>{
+//     let ano = document.getElementById("anoId").value;
+//     let mes = document.getElementById("mesId").value;
+//     let dia = document.getElementById("diaId").value;
+//     let tipo = document.getElementById("tipoId").value;
+//     let descricao = document.getElementById("descricaoId").value;
+//     let valor = document.getElementById("valorId").value;
+
+//     console.log(ano,mes,dia,tipo,descricao,valor)
+// });
+
 valoresInputs.addEventListener("click", ()=>{
-    let ano = document.getElementById("anoId").value;
-    let mes = document.getElementById("mesId").value;
-    let dia = document.getElementById("mesId").value;
-    let tipo = document.getElementById("tipoId").value;
-    let descricao = document.getElementById("descricaoId").value;
-    let valor = document.getElementById("valorId").value;
-    console.log(valoresInputs);
+    const areaInput = {
+    ano : document.getElementById("anoId").value,
+    mes : document.getElementById("mesId").value,
+    dia : document.getElementById("diaId").value,
+    tipo : document.getElementById("tipoId").value,
+    descricao : document.getElementById("descricaoId").value,
+    valor : document.getElementById("valorId").value
+    }
+
+    let getValores = new Servidor()
+    getValores.getValoresInput(areaInput);
+    // console.log(areaInput);
 });
