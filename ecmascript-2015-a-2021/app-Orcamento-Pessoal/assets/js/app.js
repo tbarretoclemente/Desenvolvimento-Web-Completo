4ea592a9d0d0633bc3daeefad2e458b92f995fe2
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-10-07 01:34:58 -0300

    atualizado

--- ecmascript-2015-a-2021/app-Orcamento-Pessoal/assets/js/app.js
+++ ecmascript-2015-a-2021/app-Orcamento-Pessoal/assets/js/app.js
@@ -9,3 +9,19 @@ const containerMenu = document.querySelector(".container--Menu");
 btnMobile.addEventListener("click", () =>{
     containerMenu.classList.toggle("show--Menu");
 });
+
+
+
+// RECUPERANDO VALORES DOS INPUT'S
+
+
+const valoresInputs = document.getElementById("btnAdd");
+valoresInputs.addEventListener("click", ()=>{
+    let ano = document.getElementById("anoId").value;
+    let mes = document.getElementById("mesId").value;
+    let dia = document.getElementById("mesId").value;
+    let tipo = document.getElementById("tipoId").value;
+    let descricao = document.getElementById("descricaoId").value;
+    let valor = document.getElementById("valorId").value;
+    console.log(valoresInputs);
+});
