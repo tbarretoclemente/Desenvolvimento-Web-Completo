3bb4d7ef0679ce7ad0e960847bbed109391fbfe1
Thiago Barreto Clemente <74058229+tbarretoclemente@users.noreply.github.com>
2022-10-08 01:07:08 -0300

    atualizado

--- ecmascript-2015-a-2021/app-Orcamento-Pessoal/assets/js/app.js
+++ ecmascript-2015-a-2021/app-Orcamento-Pessoal/assets/js/app.js
@@ -57,12 +57,32 @@ class BancoDeDados {
         localStorage.setItem(keyId, JSON.stringify(getValorInput));
         localStorage.setItem("id", keyId)
     }
+
+
+    consultaDadosLista(){
+        let arrayDespesa = [];
+        // console.log("Metodo para recuperar valores do local storage");
+
+        let consultaId = localStorage.getItem("id");
+        for(let i = 1; i <= consultaId; i++){
+            let consultaDespesa = JSON.parse(localStorage.getItem(i));
+            
+            if(consultaDespesa === null){
+                continue;
+            }
+            arrayDespesa.push(consultaDespesa);
+            // console.log(consultaDespesa);
+        }
+        return arrayDespesa;
+    }
 }
 
+let bancoDeDados = new BancoDeDados();
 // RECUPERANDO VALORES DOS INPUT'S
 
-const valoresInputs = document.getElementById("btnAdd");
-valoresInputs.addEventListener("click", ()=>{
+// CADASTRO 
+
+let valoresInputs = () =>{
     const areaInput = {
     ano : document.getElementById("anoId").value,
     mes : document.getElementById("mesId").value,
@@ -71,7 +91,7 @@ valoresInputs.addEventListener("click", ()=>{
     descricao : document.getElementById("descricaoId").value,
     valor : document.getElementById("valorId").value
     }
-
+    
     let getValores = new Despesa(
         areaInput.ano,
         areaInput.mes,
@@ -79,13 +99,13 @@ valoresInputs.addEventListener("click", ()=>{
         areaInput.tipo,
         areaInput.descricao,
         areaInput.valor);
-
-    let bancoDeDados = new BancoDeDados();
+    
+    bancoDeDados = new BancoDeDados();
     
     
     // VALIDANDO DADOS RECEBIDOS
     if(getValores.validarDados()){
-        // bancoDeDados.setLocalStorage(getValores);
+        bancoDeDados.setLocalStorage(getValores);
         console.log(`Dados Válidos`);
         alert(`Dados Válidos`);
         
@@ -93,7 +113,25 @@ valoresInputs.addEventListener("click", ()=>{
         console.log(`Dados Inválidos`)
         alert(`Dados Inválidos`);
     }
-    
-    
-});
+}
 
+
+// CONSULTA
+let consultaLista = () =>{
+    let despesas = [];
+    despesas = bancoDeDados.consultaDadosLista();
+    let listaDespesas = document.getElementById("listaDespesas");
+
+    despesas.forEach(function(d){
+        // console.log(d);
+
+        // criando linha
+        let linha = listaDespesas.insertRow(0);
+
+        // criando colunas
+        linha.insertCell(0)
+        linha.insertCell(1)
+        linha.insertCell(2)
+        linha.insertCell(3)
+    });
+}
